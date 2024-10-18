44f718755d8c7ffbc8b7d1dfc3282376a9af18f8
Abdias-Urena <abdias.urena.soto@est.una.ac.cr>
2024-10-18 00:48:34 -0600

    Portafolio Astro terminado

---
+++ src/const/const.js
@@ -0,0 +1,237 @@
+export const studies = [
+  {
+    title: "Tecnico en Informatica en Redes",
+    corporation: "Colegio Tecnico Profesional Guaycara (Rio Claro, Golfito)",
+    date: "09/2019 – 08/2021",
+  },
+  {
+    title: "Diplomado en Programación de Aplicaciones informaticas",
+    corporation: "Universidad Nacional (Costa Rica)",
+    date: "07/2021 – 22/2024",
+  },
+  {
+    title: "Bachiller Ingeniero en Sistemas de Información proximamente",
+    corporation: "Universidad Nacional (Costa Rica)",
+    date: "07/2021 – */2025",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  tailwind: "tailwind",
+  typeScript: "typeScript",
+  git: "git",
+  github: "github",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  php: "php",
+  bootstrap: "bootstrap",
+  mysql: "mysql",
+  java: "java",
+  flutter: "flutter",
+  dart: "dart",
+  google: "google",
+  firebase: "firebase",
+  processing: "processing",
+  python: "python",
+  cplusplus: "cplusplus",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "C++",
+    icon: IconKeys.cplusplus,
+  },
+  {
+    title: "Python",
+    icon: IconKeys.python,
+  },
+  {
+    title: "Processing",
+    icon: IconKeys.processing,
+  },
+  {
+    title: "Google",
+    icon: IconKeys.google,
+  },
+  {
+    title: "Firebase",
+    icon: IconKeys.firebase,
+  },
+  {
+    title: "Dart",
+    icon: IconKeys.dart,
+  },
+  {
+    title: "Flutter",
+    icon: IconKeys.flutter,
+  },
+  {
+    title: "Java",
+    icon: IconKeys.java,
+  },
+  {
+    title: "Mysql",
+    icon: IconKeys.mysql,
+  },
+  {
+    title: "Bootstrap",
+    icon: IconKeys.bootstrap,
+  },
+  {
+    title: "Php",
+    icon: IconKeys.php,
+  },
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/cardenalPos.webp",
+    title: "Cardenal POS",
+    link: "https://github.com/jocscriptch/CardenalPOS",
+    description: `Proyecto FullStack de un sistema de punto de venta, con autenticación de usuario, roles, productos, ventas, etc.`,
+    technologies: [
+      {
+        icon: IconKeys.php,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.bootstrap,
+      }
+    ],
+  },
+  {
+    img: "../../../img/SystemUniversity.webp",
+    title: "University Sports System",
+    link: "",
+    description: `Proyecto FullStack de un sistema de gestión de deportes universitarios, con autenticación de usuario, deportes, torneos, etc.`,
+    technologies: [
+      {
+        icon: IconKeys.java,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.mysql,
+      },
+    ],
+  },
+  {
+    img: "../../../img/AppMovilSismos.webp",
+    title: "Sismos App",
+    link: "https://github.com/Abdias-Urena/SismosLabFlutter",
+    description: `Proyecto de una aplicación móvil para ver los sismos en tiempo real, con notificaciones push, etc.`,
+    technologies: [
+      {
+        icon: IconKeys.dart,
+      },
+      {
+        icon: IconKeys.flutter,
+      },
+      {
+        icon: IconKeys.materialUi,
+      },
+    ],
+  },
+  {
+    img: "../../../img/EcommerceAppMovil.webp",
+    title: "Ecommerce App",
+    link: "https://github.com/Abdias-Urena/eCommerceApp",
+    description: `Proyecto de una aplicación móvil de Ecommerce, con autenticación de usuario, 
+    slider productos, mensajeria instantánea etc.`,
+    technologies: [
+      {
+        icon: IconKeys.flutter,
+      },
+      {
+        icon: IconKeys.dart,
+      },
+      {
+        icon: IconKeys.materialUi,
+      },
+      {
+        icon: IconKeys.google,
+      },
+      {
+        icon: IconKeys.firebase,
+      },
+    ],
+  },
+  {
+    img: "../../../img/CuboRubik.webp",
+    title: "Rubik's Cube",
+    link: "https://github.com/jocscriptch/Rubik-Cube",
+    description: `Proyecto de un cubo de rubik en 3D, con animaciones, movimientos, etc.`,
+    technologies: [
+      {
+        icon: IconKeys.processing,
+      },
+      {
+        icon: IconKeys.java,
+      },
+    ],
+  },
+];
